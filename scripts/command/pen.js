5045a62214e35184d431c68ad9b8519dbc433e4d
chistytsu-stack <chistytsu@gmail.com>
2025-11-10 13:07:40 +0600

    Update pen.js

--- scripts/command/pen.js
+++ scripts/command/pen.js
@@ -5,7 +5,7 @@ const path = require("path");
 module.exports = {
   config: {
     name: "pending",
-    aliases: ["pend"],
+    aliases: ["pen", "pending"],
     version: "2.0",
     author: "Meheraz Islam (Chisty)",
     countDown: 5,
